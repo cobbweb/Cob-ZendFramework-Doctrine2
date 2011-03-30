dcac549a7416c0253cb0fdb6a472693e9dbc3267
Andrew Cobby <andrew@vividpromos.com.au>
2011-03-30 17:19:54 +1000

    Ckeditor helpers

---
+++ lib/Cob/Form/Element/Ckeditor.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * My Promotions eCommerce Platform
+ * Copyright © 2010-2011 Promocon Group Pty. Ltd.
+ */
+namespace Cob\Form\Element;
+
+/**
+ * 
+ * 
+ * @author Cobby
+ */
+
+class Ckeditor extends \Zend_Form_Element_Xhtml
+{
+
+    public $helper = 'ckeditor';
+
+}
---
+++ lib/Cob/View/Helper/Ckeditor.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * My Promotions eCommerce Platform
+ * Copyright © 2010-2011 Promocon Group Pty. Ltd.
+ */
+namespace Cob\View\Helper;
+
+/**
+ * 
+ * 
+ * @author Cobby
+ */
+
+class Ckeditor extends \Zend_View_Helper_FormTextarea
+{
+    public function ckeditor($name, $value = null, $attribs = null)
+    {
+        $info = $this->_getInfo($name, $value, $attribs);
+        $this->view->headScript()->appendFile($this->view->payloadUrl('js/ckeditor/ckeditor.js', 'core'));
+        $this->view->headScript()->appendFile($this->view->payloadUrl('js/ckeditor/adapters/jquery.js', 'core'));
+        $html = parent::formTextarea($name, $value, $attribs);
+        $html .= <<<CKE
+\n
+<script type="text/javascript">
+$('#{$info['id']}').ckeditor();
+</script>
+CKE;
+        return $html;
+    }
+
+
+}
